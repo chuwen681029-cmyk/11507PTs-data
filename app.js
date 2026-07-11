8a8524b214d41b626f9210ec78fdbbdb105d49b9
chuwen681029-cmyk <chuwen681029@gmail.com>
2026-07-11 17:42:22 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function gisLoaded() {
     client_id: CONFIG.CLIENT_ID,
     scope: [
       "https://www.googleapis.com/auth/spreadsheets",
-      "https://www.googleapis.com/auth/drive.file",
+      "https://www.googleapis.com/auth/drive",
       "https://www.googleapis.com/auth/userinfo.email",
       "https://www.googleapis.com/auth/userinfo.profile"
     ].join(" "),
